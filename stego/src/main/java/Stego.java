6236f6d2579c299c2e307edc54126c0fe9afe43d
JuandeMata <matalmagro@gmail.com>
2020-05-06 19:18:06 +0200

    test

--- stego/src/main/java/Stego.java
+++ stego/src/main/java/Stego.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Stego {
 
     private static final int BITS_PIXEL_POSITION = 28;
@@ -8,7 +15,9 @@ public class Stego {
     private static final int HEIGHT_SIZE = 4;
 
     public byte[] encodeTextIntoImage(byte[] inputImage, byte[] text) {
-
+        if (!isPossibleToHide(getBitsPerPixel(inputImage), getImageWidth(inputImage))) {
+            throw new IllegalArgumentException("Not possible to hide info");
+        }
         return null;
     }
 
@@ -28,12 +37,21 @@ public class Stego {
         return image[BITS_PIXEL_POSITION + 1] * 8 + image[BITS_PIXEL_POSITION];
     }
 
-    private long getWidth(byte[] image) {
+    private long getImageWidth(byte[] image) {
         return image[WIDTH_POSITION] + (image[WIDTH_POSITION + 1] * 8) + (image[WIDTH_POSITION + 2] * 8 * 8) + (image[WIDTH_POSITION + 3] * 8 * 8 * 8);
     }
 
-    private long getHeight(byte[] image) {
+    private long getImageHeight(byte[] image) {
         return image[HEIGHT_POSITION] + (image[HEIGHT_POSITION + 1] * 8) + (image[HEIGHT_POSITION + 2] * 8 * 8) + (image[HEIGHT_POSITION + 3] * 8 * 8 * 8);
+    }
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        Path path = Paths.get(Stego.class.getClassLoader()
+                .getResource("foto.bmp").toURI());
+
+        byte[] patata = Files.readAllBytes(path);
+
+        System.out.println(patata);
     }
 
 }
